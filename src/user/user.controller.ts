07b36fa0c332a330ba2c28f274a10dd1f6dd3743
Leonardo de Brito <leonardode.b.c@gmail.com>
2026-08-06 17:13:32 -0300

    added ConfigModule for global use and db typeorm with choose betweeen better-sqlite3 and postgres

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,15 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CustomParseIntPipe } from '../common/pipes/custom-parse-int-pipe.pipe';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly configService: ConfigService) {}
+
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', CustomParseIntPipe) id: number) {
+    console.log(process.env.TESTE1 || 'Padrão');
+    console.log(this.configService.getOrThrow('TESTE1'));
     return `Olá do controller do user #${id}`;
   }
 }
