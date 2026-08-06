4c36c9d87e516f95576c98003461d6b75da216cd
Leonardo de Brito <leonardode.b.c@gmail.com>
2026-08-05 21:15:23 -0300

    added validation pipe and class validator for request data validation

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class LoginDto {
-  email?: string;
-  password?: string;
+  @IsEmail({}, { message: 'E-mail inválido' })
+  email!: string;
+
+  @IsString({ message: 'Senha precisa ser uma string' })
+  @IsNotEmpty({ message: 'Senha não pode estar vazia' })
+  password!: string;
 }
