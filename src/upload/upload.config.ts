9e493aefeaefde6c1f251984a5df2d1a5a9d75a2
Leonardo de Brito <leonardode.b.c@gmail.com>
2026-09-01 21:53:30 -0300

    create upload image with servestaticmodule, multer, filetype and more for security

---
+++ src/upload/upload.config.ts
@@ -0,0 +1,20 @@
+import { memoryStorage } from 'multer';
+import { BadRequestException } from '@nestjs/common';
+
+export const storage = memoryStorage();
+
+export const fileFilter = (
+  req: any,
+  file: Express.Multer.File,
+  cb: (error: Error | null, acceptFile: boolean) => void,
+) => {
+  if (!file.mimetype.startsWith('image/')) {
+    return cb(
+      new BadRequestException('Somente imagens são permitidas!'),
+      false,
+    );
+  }
+  cb(null, true);
+};
+
+export const limits = {};
